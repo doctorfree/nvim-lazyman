bc18640ea3a1576449dc2e434fd75630faef56fd
Ronald Record <github@ronrecord.com>
2023-03-10 13:21:18 -0800

    Add Alpha autocmds, load on VimEnter

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,4 +1,4 @@
--- This file is automatically loaded by plugins.init
+local settings = require("configuration")
 
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyman_" .. name, { clear = true })
@@ -68,3 +68,48 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+if settings.enable_alpha then
+  local alpha_group = api.nvim_create_augroup("alpha_autocmd", { clear = true })
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "AlphaReady",
+    group = alpha_group,
+    callback = function()
+      require("lualine").hide({
+        place = { "statusline", "tabline", "winbar" }, -- the segment this change applies to.
+        unhide = false, -- whether to re-enable lualine again/
+      })
+    end
+  })
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "AlphaReady",
+    group = alpha_group,
+    callback = function()
+      vim.cmd([[
+        setlocal showtabline=0 | autocmd BufUnload <buffer> set showtabline=1
+        setlocal laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+      ]])
+    end,
+  })
+
+  -- when there is no buffer left show Alpha dashboard
+  -- requires "famiu/bufdelete.nvim" and "goolord/alpha-nvim"
+  api.nvim_create_autocmd("User", {
+    pattern = "BDeletePost*",
+    group = alpha_group,
+    callback = function(event)
+      local fallback_name = vim.api.nvim_buf_get_name(event.buf)
+      local fallback_ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
+      local fallback_on_empty = fallback_name == "" and fallback_ft == ""
+
+      if fallback_on_empty then
+        -- require("neo-tree").close_all()
+        vim.api.nvim_command("Alpha")
+        vim.api.nvim_command(event.buf .. "bwipeout")
+      end
+    end,
+  })
+end
+
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,8 +2,7 @@ local settings = require('configuration')
 
 local dashboard_type = {
   "glepnir/dashboard-nvim",
-  lazy = false,
-  -- event = "VimEnter",
+  event = "VimEnter",
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
   keys = { { "<leader>0", "<cmd>Dashboard<CR>", desc = "Dashboard" } },
   config = function()
@@ -15,8 +14,7 @@ if settings.enable_alpha then
   dashboard_type = {
     {
       "goolord/alpha-nvim",
-      lazy = false,
-      -- event = "VimEnter",
+      event = "VimEnter",
       keys = { { "<leader>a", "<cmd>Alpha<cr>", "Alpha" } },
       config = function() require("config.alpha.alpha") end,
     },
