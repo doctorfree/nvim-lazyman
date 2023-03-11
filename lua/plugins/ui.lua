06c08ee13361ac8ed4f15724edc3a44ae6d14ff8
Ronald Record <github@ronrecord.com>
2023-03-10 19:06:09 -0800

    Configure indent plugins with filetypes, enable Alpha

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -165,33 +165,57 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+      char = "▏",
+      context_char = "▏",
+      show_end_of_line = false,
+      space_char_blankline = " ",
+      show_current_context = true,
+      show_current_context_start = true,
+      filetype_exclude = {
+        "help",
+        "startify",
+        "dashboard",
+        "packer",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+        "alpha",
+        "neo-tree",
+      },
+      buftype_exclude = {
+        "terminal",
+        "nofile",
+      },
     },
   },
 
-  -- active indent guide and indent text objects
   {
     "echasnovski/mini.indentscope",
+    lazy = true,
+    enabled = true,
+    -- lazy = true,
     version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = { "BufReadPre", "BufNewFile" },
+    -- event = "BufReadPre",
     opts = {
-      -- symbol = "▏",
-      symbol = "│",
-      options = { try_as_border = true },
+      symbol = "▏",
+      -- symbol = "│",
+      options = { try_as_border = false },
     },
-    init = function()
+    config = function(_, opts)
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = {
+          "help",
+          "dashboard",
+          "alpha",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+        },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
       })
-    end,
-    config = function(_, opts)
       require("mini.indentscope").setup(opts)
     end,
   },
