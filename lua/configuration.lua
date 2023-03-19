7c2b59618e4521fbf662616914e397320d9f41e0
Ronald Record <github@ronrecord.com>
2023-03-19 10:20:39 -0700

    Organize sections in configuration.lua

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -1,4 +1,6 @@
 local M = {}
+
+-- THEME CONFIGURATION
 -- Available themes:
 --   monokai-pro, nightfox, tokyonight, kanagawa, catppuccin, tundra, onedarkpro, everforest
 -- A configuration file for each theme is in lua/themes/
@@ -12,19 +14,15 @@ M.theme = "tokyonight"
 --   catppuccin:  latte, frappe, macchiato, mocha
 --   nightfox:    carbonfox, dawnfox, dayfox, duskfox, nightfox, nordfox, terafox
 M.theme_style = "moon"
+-- enable transparency if the theme supports it
+M.enable_transparent = true
+
+-- GLOBAL OPTIONS CONFIGURATION
 -- Some prefer space as the map leader, but why
 M.mapleader = ","
 M.maplocalleader = ","
--- enable transparency if the theme supports it
-M.enable_transparent = true
 -- Toggle global status line
 M.global_statusline = true
--- use rg instead of grep
-M.grepprg = "rg --hidden --vimgrep --smart-case --"
--- media backend, one of "ueberzug"|"viu"|"chafa"|"jp2a"|catimg
-M.media_backend = "jp2a"
--- neovim session manager to use, either persistence or possession
-M.session_manager = "possession"
 -- set numbered lines
 M.number = false
 -- enable mouse see :h mouse
@@ -38,6 +36,12 @@ M.showtabline = 2
 M.list = true
 -- which list chars to show
 M.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<"
+-- use rg instead of grep
+M.grepprg = "rg --hidden --vimgrep --smart-case --"
+
+-- ENABLE/DISABLE/SELECT PLUGINS
+-- neovim session manager to use, either persistence or possession
+M.session_manager = "possession"
 -- neo-tree or nvim-tree, false will enable nvim-tree
 M.enable_neotree = true
 -- Noice heavily changes the Neovim UI ...
@@ -57,13 +61,6 @@ M.enable_winbar = false
 M.enable_games = true
 -- Enable the Alpha dashboard
 M.enable_alpha = true
--- Number of recent files shown in dashboard
--- 0 disables showing recent files
-M.dashboard_recent_files = 5
--- disable the header of the dashboard
-M.disable_dashboard_header = false
--- disable quick links of the dashboard
-M.disable_dashboard_quick_links = false
 -- enable the Neovim bookmarks plugin (https://github.com/ldelossa/nvim-ide)
 M.enable_bookmarks = false
 -- enable the Neovim IDE plugin (https://github.com/ldelossa/nvim-ide)
@@ -74,6 +71,17 @@ M.enable_navigator = true
 M.enable_project = true
 -- Enable window picker
 M.enable_picker = true
+
+-- PLUGINS CONFIGURATION
+-- media backend, one of "ueberzug"|"viu"|"chafa"|"jp2a"|catimg
+M.media_backend = "jp2a"
+-- Number of recent files shown in dashboard
+-- 0 disables showing recent files
+M.dashboard_recent_files = 5
+-- disable the header of the dashboard
+M.disable_dashboard_header = false
+-- disable quick links of the dashboard
+M.disable_dashboard_quick_links = false
 -- treesitter parsers to be installed
 -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 M.treesitter_ensure_installed = "maintained"
