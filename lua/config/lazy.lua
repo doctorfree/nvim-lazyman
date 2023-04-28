8d7481494ea651c0ab6b1cb8871f85c301a3c565
Ronald Record <github@ronrecord.com>
2023-04-28 10:44:24 -0700

    Cleanup lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,15 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 local settings = require("configuration")
 
@@ -15,7 +20,6 @@ require("lazy").setup({
   defaults = {
     lazy = true,
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = {
     colorscheme = { settings.theme, "habamax" },
@@ -32,9 +36,6 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
