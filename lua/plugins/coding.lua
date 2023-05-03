c1b4fe3e563d72a87870eb635f46ef695916c015
Ronald Record <github@ronrecord.com>
2023-05-03 09:18:27 -0700

    Replace mini.surround with nvim-surround

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,7 +5,7 @@ return {
     "L3MON4D3/LuaSnip",
     build = (not jit.os:find("Windows"))
         and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-      or nil,
+        or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
@@ -56,12 +56,12 @@ return {
     event = "VeryLazy",
     opts = {
       enable_check_bracket_line = false, -- Don't add pairs if it already has a close pair in the same line
-      ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
-      check_ts = true, -- use treesitter to check for a pair.
+      ignored_next_char = "[%w%.]",      -- will ignore alphanumeric and `.` symbol
+      check_ts = true,                   -- use treesitter to check for a pair.
       ts_config = {
-        lua = { "string" }, -- it will not add pair on that treesitter node
+        lua = { "string" },              -- it will not add pair on that treesitter node
         javascript = { "template_string" },
-        java = false, -- don't check treesitter on java
+        java = false,                    -- don't check treesitter on java
       },
     },
     -- config = function() require("nvim-autopairs").setup {} end
@@ -72,40 +72,10 @@ return {
 
   -- surround
   {
-    "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "sa", -- Add surrounding in Normal and Visual modes
-        delete = "sd", -- Delete surrounding
-        find = "sf", -- Find surrounding (to the right)
-        find_left = "sF", -- Find surrounding (to the left)
-        highlight = "sh", -- Highlight surrounding
-        replace = "sr", -- Replace surrounding
-        update_n_lines = "sn", -- Update `n_lines`
-        suffix_last = "l", -- Suffix to search with "prev" method
-        suffix_next = "n", -- Suffix to search with "next" method
-      },
-    },
-    config = function(_, opts)
-      require("mini.surround").setup(opts)
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
     end,
   },
 
