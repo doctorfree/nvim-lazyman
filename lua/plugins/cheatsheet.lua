51df0fc55ddf60a0993f645944a865ced003627c
Ronald Record <github@ronrecord.com>
2023-05-03 16:26:47 -0700

    Add cheatsheet plugin and custom cheatsheet.txt

---
+++ lua/plugins/cheatsheet.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "sudormrfbin/cheatsheet.nvim",
+    lazy = false,
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      local ctactions = require("cheatsheet.telescope.actions")
+      require("cheatsheet").setup({
+        bundled_cheatsheets = true,
+        bundled_plugin_cheatsheets = true,
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ["<CR>"] = ctactions.select_or_fill_commandline,
+          ["<A-CR>"] = ctactions.select_or_execute,
+          ["<C-Y>"] = ctactions.copy_cheat_value,
+          ["<C-E>"] = ctactions.edit_user_cheatsheet,
+        },
+      })
+    end,
+  },
+}
