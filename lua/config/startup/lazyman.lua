c9fe716dccf65dfb2bb272d39172b68b6379c30e
Ronald Record <github@ronrecord.com>
2023-05-05 08:42:38 -0700

    Add Lazyman header and footer to startup lazyman theme

--- lua/config/startup/lazyman.lua
+++ lua/config/startup/lazyman.lua
@@ -1,3 +1,13 @@
+local datetime = os.date("  %Y-%b-%d   %H:%M:%S", os.time())
+local version = vim.version()
+local version_info = ""
+if version ~= nil then
+  version_info = "v" .. version.major .. "." .. version.minor .. "." .. version.patch
+end
+
+local stats = require("lazy").stats()
+local vinfo = "Neovim " .. version_info
+local lazystats = "  loaded " .. stats.count .. " plugins "
 local settings = {
   -- every line should be same width without escaped \
   header = {
@@ -8,12 +18,13 @@ local settings = {
     title = "Header",
     margin = 5,
     content = {
-      " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-      " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-      " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-      " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-      " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-      " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+      "  _                                            ",
+      " | |                                           ",
+      " | |  __,   __         _  _  _    __,   _  _   ",
+      " |/  /  |  / / _|   | / |/ |/ |  /  |  / |/ |  ",
+      " |__/\\_/|_/ /_/  \\_/|/  |  |  |_/\\_/|_/  |  |_/",
+      "             /|    /|                          ",
+      "             \\|    \\|                          ",
     },
     highlight = "Statement",
     default_color = "",
@@ -54,7 +65,7 @@ local settings = {
     fold_section = false,
     title = "Footer",
     margin = 5,
-    content = { "Lazyman Neovim with Startup Dashboard" },
+    content = { datetime .. "  " .. vinfo .. lazystats },
     highlight = "Number",
     default_color = "",
     oldfiles_amount = 0,
@@ -65,7 +76,7 @@ local settings = {
     cursor_column = 0.5,
     empty_lines_between_mappings = false,
     disable_statuslines = true,
-    paddings = { 1, 1, 1, 0 },
+    paddings = { 0, 1, 1, 0 },
   },
   mappings = {
     execute_command = "<CR>",
