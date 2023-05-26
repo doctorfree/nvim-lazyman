2fd825c09185f877aabd5d91a1d8f7bec66eb3da
Ronald Record <github@ronrecord.com>
2023-05-26 11:06:57 -0700

    Change GPT model download to vicuna

--- scripts/gpt4all.sh
+++ scripts/gpt4all.sh
@@ -3,7 +3,8 @@
 # Download the GPT4ALL model used by codeexplain.nvim
 #
 # Set this to the model and version to download
-MODEL_URL="https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
+# MODEL_URL="https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
+MODEL_URL="https://gpt4all.io/models/ggml-vicuna-7b-1.1-q4_2.bin"
 
 have_curl=$(type -p curl)
 [ "$have_curl" ] || {
