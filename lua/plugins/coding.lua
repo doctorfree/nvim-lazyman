333093092526d661947c5c4289adbbfe8a034850
Ronald Record <github@ronrecord.com>
2023-06-10 12:01:03 -0700

    Fix copilot cmp loading error

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,126 @@
 local settings = require("configuration")
+local enable_copilot = settings.enable_copilot
+local enable_neoai = settings.enable_neoai
+if not settings.enable_coding then
+  enable_copilot = false
+  enable_neoai = false
+end
+
+local copilot = {}
+local copilot_cmp = {}
+local neoai = {}
+if enable_neoai then
+  neoai = {
+    "Bryley/neoai.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+      "NeoAI",
+      "NeoAIOpen",
+      "NeoAIClose",
+      "NeoAIToggle",
+      "NeoAIContext",
+      "NeoAIContextOpen",
+      "NeoAIContextClose",
+      "NeoAIInject",
+      "NeoAIInjectCode",
+      "NeoAIInjectContext",
+      "NeoAIInjectContextCode",
+    },
+    keys = {
+      { "<leader>as", desc = "summarize text" },
+      { "<leader>ag", desc = "generate git message" },
+    },
+    config = function()
+      require("neoai").setup({
+        ui = {
+          output_popup_text = "NeoAI",
+          input_popup_text = "Prompt",
+          width = 30,      -- As percentage eg. 30%
+          output_popup_height = 80, -- As percentage eg. 80%
+          submit = "<Enter>", -- Key binding to submit the prompt
+        },
+        models = {
+          {
+            name = "openai",
+            model = "gpt-3.5-turbo",
+            params = nil,
+          },
+        },
+        register_output = {
+          ["g"] = function(output)
+            return output
+          end,
+          ["c"] = require("neoai.utils").extract_code_snippets,
+        },
+        inject = {
+          cutoff_width = 75,
+        },
+        prompts = {
+          context_prompt = function(context)
+            return "Hey, I'd like to provide some context for future "
+              .. "messages. Here is the code/text that I want to refer "
+              .. "to in our upcoming conversations:\n\n"
+              .. context
+            end,
+        },
+        mappings = {
+          ["select_up"] = "<C-k>",
+          ["select_down"] = "<C-j>",
+        },
+        open_api_key_env = "OPENAI_API_KEY",
+        shortcuts = {
+          {
+            name = "textify",
+            key = "<leader>as",
+            desc = "fix text with AI",
+            use_context = true,
+            prompt = [[
+              Please rewrite the text to make it more readable, clear,
+              concise, and fix any grammatical, punctuation, or spelling
+              errors
+            ]],
+            modes = { "v" },
+            strip_function = nil,
+          },
+          {
+            name = "gitcommit",
+            key = "<leader>ag",
+            desc = "generate git commit message",
+            use_context = false,
+            prompt = function ()
+              return [[
+                Using the following git diff generate a consise and
+                clear git commit message, with a short title summary
+                that is 75 characters or less:
+              ]] .. vim.fn.system("git diff --cached")
+            end,
+            modes = { "n" },
+            strip_function = nil,
+          },
+        },
+      })
+    end,
+  }
+end
+if enable_copilot then
+  copilot = {
+    "zbirenbaum/copilot.lua",
+    build = ":Copilot auth",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("config.copilot")
+    end,
+  }
+  copilot_cmp = {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  }
+end
 
 local cmpnpm = {}
 if not settings.enable_copilot then
@@ -91,6 +213,7 @@ if settings.enable_coding then
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-calc",
+      copilot_cmp,
     },
     config = function()
       require("config.nvim-cmp")
@@ -166,14 +289,12 @@ return {
   },
 
   surround,
-
   minicomment,
-
   inlayhints,
-
   signature,
-
   lspsaga,
-
   actionmenu,
+  -- AI
+  copilot,
+  neoai,
 }
