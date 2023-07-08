6d1784d6ce37a0e7997a4aa90bd1bd8b55f4f6f9
Ronald Record <github@ronrecord.com>
2023-07-08 10:58:38 -0700

    Improve nvim kill script to catch headless and bob invocations

--- scripts/kill_all_neovim.sh
+++ scripts/kill_all_neovim.sh
@@ -5,3 +5,13 @@ do
   pid=$(echo "$l" | awk ' { print $2 } ')
   kill ${pid}
 done
+ps -ef | grep 'nvim' | grep 'headless' | grep -v grep | while read l
+do
+  pid=$(echo "$l" | awk ' { print $2 } ')
+  kill ${pid}
+done
+ps -ef | grep 'nvim' | grep 'local/share/bob' | grep -v grep | while read l
+do
+  pid=$(echo "$l" | awk ' { print $2 } ')
+  kill ${pid}
+done
