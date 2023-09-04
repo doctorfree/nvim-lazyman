a62b5f687cd0aff7fdcf7a9815e4e3f4efdc274c
Ronald Record <github@ronrecord.com>
2023-09-04 16:00:03 -0700

    Align dashboard search recents

--- lua/candy/plugins/alpha.lua
+++ lua/candy/plugins/alpha.lua
@@ -154,7 +154,7 @@ local function mru(start, cwd, items_number, opts)
   }
 end
 
-local recent_files_btn = dashboard.button("r", candy_icons.fileRecent .. " " .. "Search recents", "<cmd>Telescope oldfiles hidden=true prompt_title=Recent<CR>")
+local recent_files_btn = dashboard.button("r", " Search recents", "<cmd>Telescope oldfiles hidden=true prompt_title=Recent<CR>")
 recent_files_btn.opts.hl = "AlphaButtons"
 
 local section_mru = {
