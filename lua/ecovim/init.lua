47635f2af5a5eedea446709300454b500e925ff4
Ronald Record <github@ronrecord.com>
2023-09-08 10:23:17 -0700

    Pull autocmds, keymappings, options in ecovim namespace up a level. Rename keymappings to keymaps

--- lua/ecovim/init.lua
+++ lua/ecovim/init.lua
@@ -3,10 +3,10 @@ require('ecovim.config.Ecovim')
 require('ecovim.utils.globals')
 require('ecovim.utils.functions')
 
-require('ecovim.config.options')
+require('ecovim.options')
 require('ecovim.config.lazy')
-require('ecovim.config.keymappings')
-require('ecovim.config.autocmds')
+require('ecovim.keymaps')
+require('ecovim.autocmds')
 
 require('ecovim.internal.winbar')
 require('ecovim.internal.cursorword')
