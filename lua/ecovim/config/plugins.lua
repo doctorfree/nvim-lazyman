2853b67858f849f2070892f020213ce7cdce342b
Ronald Record <github@ronrecord.com>
2023-09-10 15:01:46 -0700

    Use config/null-ls.lua to configure ecovim namespace null-ls

--- lua/ecovim/config/plugins.lua
+++ lua/ecovim/config/plugins.lua
@@ -347,40 +347,19 @@ if settings.enable_coding then
     {
       "jose-elias-alvarez/null-ls.nvim",
       event = { "BufReadPre", "BufNewFile" },
-      dependencies = { "mason.nvim" },
+      dependencies = {
+        "neovim/nvim-lspconfig",
+        "mason.nvim",
+      },
       config = function()
-        local null_ls = require("null-ls")
-        local formatting = null_ls.builtins.formatting
-        local diagnostics = null_ls.builtins.diagnostics
-        null_ls.setup({
-          debug = false,
-          -- You can then register sources by passing a sources list into your setup function:
-          -- using `with()`, which modifies a subset of the source's default options
-          sources = {
-            formatting.prettier,
-            formatting.stylua,
-            formatting.markdownlint,
-            formatting.beautysh.with({ extra_args = { "--indent-size", "2" } }),
-            formatting.black,
-            diagnostics.flake8.with({ extra_args = { "--ignore=E203,E501,E402,F401,F821,W503,W292" }, filetypes = { "python" } }),
-          },
-        })
+        require("config.null-ls")
       end,
     },
-
     {
       "jay-babu/mason-null-ls.nvim",
       event = { "BufReadPre", "BufNewFile" },
       opts = {
-        ensure_installed = {
-          "prettier",
-          "stylua",
-          "google_java_format",
-          "black",
-          "flake8",
-          "markdownlint",
-          "beautysh",
-        },
+        ensure_installed = formatters_linters,
         automatic_setup = true,
       },
     },
