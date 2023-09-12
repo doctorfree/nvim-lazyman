29b6e66d65bd0ef09e5b7a6d00fd18423c137fc6
Ronald Record <github@ronrecord.com>
2023-09-12 08:03:05 -0700

    Use same config for nvim-lspconfig in ecovim and free namespaces

--- lua/ecovim/config/plugins.lua
+++ lua/ecovim/config/plugins.lua
@@ -263,128 +263,48 @@ if settings.enable_coding then
       opts = {},
     },
     {
-      "neovim/nvim-lspconfig",
-      branch = "master",
-      event = { "BufReadPre", "BufNewFile" },
+      "williamboman/mason.nvim",
+      build = ":MasonUpdate",
+      cmd = { "Mason", "MasonUpdate", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+      lazy = false,
+      keys = { { "<leader>M", "<cmd>Mason<cr>", desc = "Mason Menu" } },
+    },
+
+    {
+      "williamboman/mason-lspconfig.nvim",
       dependencies = {
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
       },
-      keys = {
-        { "<leader>de", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Open float" }},
-        { "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true }},
-        { "]d", vim.diagnostic.goto_next, { noremap = true, silent = true }},
-        { "<leader>dq", vim.diagnostic.setloclist, { noremap = true, silent = true, desc = "Set diagnostics location list" }},
-        { "<leader>dt", toggle_diagnostics, { desc = "Toggle diagnostics" }},
-        { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action" },
-        { "<leader>ld", "<cmd>Telescope lsp_document_diagnostics<cr>", desc = "Document Diagnostics" },
-        { "<leader>lw", "<cmd>Telescope lsp_workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
-        { "<leader>li", "<cmd>LspInfo<cr>", desc = "Info" },
-        { "<leader>lI", "<cmd>LspInstallInfo<cr>", desc = "Installer Info" },
-        { "<leader>lj", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", desc = "Next Diagnostic" },
-        { "<leader>lk", "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", desc = "Prev Diagnostic" },
-        { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", desc = "CodeLens Action" },
-        { "<leader>lq", "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", desc = "Quickfix" },
-        { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename" },
-        { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
-        { "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols" },
-        {
-          "<leader>W",
-          function()
-            vim.lsp.buf.format({
-              filter = function(client)
-                -- do not use default `lua_ls` to format
-                local exclude_servers = { "lua_ls" }
-                return not vim.tbl_contains(exclude_servers, client.name)
-              end,
-            })
-            vim.cmd([[w!]])
-          end,
-          desc = "Format and Save",
-        },
-      },
+    },
+
+    {
+      "RubixDev/mason-update-all",
+      cmd = "MasonUpdateAll",
       config = function()
-        -- special attach lsp
-        require("util").on_attach(function(client, buffer)
-          require("config.lsp.navic").attach(client, buffer)
-          require("config.lsp.lspkeymaps").attach(client, buffer)
-          require("config.lsp.inlayhints").attach(client, buffer)
-          require("config.lsp.gitsigns").attach(client, buffer)
-          require("config.lsp.python").attach(client, buffer)
-        end)
-
-        local showdiag = settings.show_diagnostics
-
-        local open_float = "<cmd>lua vim.diagnostic.open_float()<cr>"
-        if not showdiag == "popup" then
-          open_float = ""
-        end
-
-        -- Style floating windows
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-
-        -- Borders for LspInfo winodw
-        require("lspconfig.ui.windows").default_options.border = "rounded"
-
-        -- diagnostics
-        for name, icon in pairs(require("icons").diagnostics) do
-          local function firstUpper(s)
-            return s:sub(1, 1):upper() .. s:sub(2)
-          end
-          name = "DiagnosticSign" .. firstUpper(name)
-          vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-        end
-        if showdiag == "none" then
-          vim.diagnostic.config(require("config.lsp.diagnostics")["off"])
-        else
-          vim.diagnostic.config(require("config.lsp.diagnostics")["on"])
-        end
-
-        -- Show line diagnostics automatically in hover window
-        if showdiag == "popup" then
-          vim.cmd([[
-            autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })
-          ]])
-        end
-
-        local servers = require("config.lsp.lspservers")
-        local ext_capabilities = vim.lsp.protocol.make_client_capabilities()
-        local capabilities = require("util").capabilities(ext_capabilities)
-
-        local function setup(server)
-          if servers[server] and servers[server].disabled then
-            return
-          end
-          local server_opts = vim.tbl_deep_extend("force", {
-            capabilities = vim.deepcopy(capabilities),
-          }, servers[server] or {})
-          require("lspconfig")[server].setup(server_opts)
-        end
-
-        local available = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-
-        for server, server_opts in pairs(servers) do
-          if server_opts then
-            if not vim.tbl_contains(available, server) then
-              setup(server)
-            end
-          end
-        end
-
-        require("mason-lspconfig").setup({
-          ensure_installed = lsp_servers,
-          automatic_installation = true,
+        require("mason-update-all").setup()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "MasonUpdateAllComplete",
+          callback = function()
+            print("mason-update-all has finished")
+          end,
         })
-        require("mason-lspconfig").setup_handlers({ setup })
       end,
     },
 
     {
-      "williamboman/mason.nvim",
-      cmd = { "Mason", "MasonUpdate", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "jose-elias-alvarez/nvim-lsp-ts-utils",
+        "jose-elias-alvarez/null-ls.nvim",
+        "nvim-lua/plenary.nvim",
+        "b0o/schemastore.nvim",
+        "folke/neodev.nvim",
+      },
       config = function()
         local opts = {
+          ensure_installed = formatters_linters,
           ui = {
             border = "rounded",
             icons = {
@@ -395,6 +315,25 @@ if settings.enable_coding then
           },
         }
         require("mason").setup(opts)
+        local mr = require("mason-registry")
+        local function install_ensured()
+          for _, tool in ipairs(opts.ensure_installed) do
+            local p = mr.get_package(tool)
+            if not p:is_installed() then
+              p:install()
+            end
+          end
+        end
+        if mr.refresh then
+          mr.refresh(install_ensured)
+        else
+          install_ensured()
+        end
+        require("mason-lspconfig").setup({
+          ensure_installed = lsp_servers,
+          automatic_installation = true,
+        })
+        require("config.lspconfig")
       end,
     },
 
@@ -416,13 +355,6 @@ if settings.enable_coding then
       opts = {
         ensure_installed = formatters_linters,
         automatic_setup = true,
-      },
-    },
-
-    {
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = {
-        "williamboman/mason.nvim",
       },
     },
 
