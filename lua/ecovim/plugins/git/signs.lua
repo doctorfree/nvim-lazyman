37fa1bbff0cff7b5654d4fce6341fbcea4d7374d
ron.record <ron.record@hornblower.com>
2024-06-23 09:10:21 -0700

    Fix gitsigns deprecation warnings in ecovim namespace

--- lua/ecovim/plugins/git/signs.lua
+++ lua/ecovim/plugins/git/signs.lua
@@ -7,14 +7,6 @@ end
 -- │ Setup                                                    │
 -- ╰──────────────────────────────────────────────────────────╯
 signs.setup {
-  signs                        = {
-    add          = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change       = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-  },
   signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -43,9 +35,6 @@ signs.setup {
     relative = 'cursor',
     row = 0,
     col = 1
-  },
-  yadm                         = {
-    enable = false
   },
   on_attach                    = function(bufnr)
     local gs = package.loaded.gitsigns
